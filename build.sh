4f67aedcc84ecc0ed06d433beab19dd16326fa06
Bubba <rob@stechstudio.com>
2017-05-05 16:16:15 -0400

    Docker all the build things.

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+if [ $# -lt 1 ]; then
+  echo
+  echo "Usage: $0 VERSION"
+  echo "Build shared libraries for libvips and its dependencies via containers"
+  echo
+  echo "Please specify the libvips VERSION, e.g. 8.3.3"
+  echo
+  exit 1
+fi
+VERSION_VIPS="$1"
+
+# Is docker available?
+if ! type docker >/dev/null; then
+  echo "Please install docker"
+  exit 1
+fi
+
+echo "Building vips..."
+docker build -t vips-dev-linux-x64 linux-x64
+docker run --rm -e "VERSION_VIPS=${VERSION_VIPS}" -v $PWD:/packaging vips-dev-linux-x64 sh -c "/packaging/build/vips.sh"
