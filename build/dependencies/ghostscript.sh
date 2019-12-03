4d867a57d73cf8727a96fa128bc5d145757fdc42
Adam Bolte <adam.bolte@sitepoint.com>
2019-12-04 10:43:22 +1100

    Revert "Ghostscript latest snapshot"
    This reverts commit 2fb9e336c4605fadd380662a028c2fcf129d1775.

--- build/dependencies/ghostscript.sh
+++ build/dependencies/ghostscript.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 
-#fetchSource ghostscript https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs921/ghostscript-${VERSION_GHOSTSCRIPT}.tar.gz
-
-export DEP_NAME=ghostscript
-build_dir=${DEPS}/${DEP_NAME}
-git clone http://git.ghostscript.com/ghostpdl.git ${build_dir}
-cd ${build_dir}
-VERSION_GHOSTSCRIPT=$(git rev-parse --short HEAD)
-
+fetchSource ghostscript https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs921/ghostscript-${VERSION_GHOSTSCRIPT}.tar.gz
 export JSON_VERSIONS="${JSON_VERSIONS}, \"${DEP_NAME}\": \"${VERSION_GHOSTSCRIPT}\""
 
 if [ ! -f "configured.sts" ]; then
     printf "\tConfiguring\n"
     rm -rf freetype lcms2 jpeg libpng
 
-    ./autogen.sh  \
+    ./configure  \
         --prefix=${TARGET} \
         --disable-compile-inits \
         --enable-dynamic        \
@@ -27,14 +20,15 @@ fi
 
 if [ ! -f "made.sts" ]; then
     printf "\tBuilding\n"
+    make so   >> ${BUILD_LOGS}/${DEP_NAME}.make.log 2>&1
     make install   >> ${BUILD_LOGS}/${DEP_NAME}.make.log 2>&1
-    mkdir -p ${TARGET}/include/ghostscript
+    make soinstall   >> ${BUILD_LOGS}/${DEP_NAME}.make.log 2>&1
     install -v -m644 base/*.h ${TARGET}/include/ghostscript   >> ${BUILD_LOGS}/${DEP_NAME}.make.log 2>&1
 
     mkdir -p ${TARGET}/share/ghostscript
     curl -Ls http://downloads.sourceforge.net/gs-fonts/ghostscript-fonts-std-8.11.tar.gz | tar xzC ${TARGET}/share/ghostscript   >> ${BUILD_LOGS}/${DEP_NAME}.make.log 2>&1
     curl -Ls http://downloads.sourceforge.net/gs-fonts/gnu-gs-fonts-other-6.0.tar.gz | tar xzC ${TARGET}/share/ghostscript   >> ${BUILD_LOGS}/${DEP_NAME}.make.log 2>&1
-    /usr/bin/fc-cache -v ${TARGET}/share/ghostscript/fonts/  >> ${BUILD_LOGS}/${DEP_NAME}.make.log 2>&1
+    ${TARGET}/bin/fc-cache -v ${TARGET}/share/ghostscript/fonts/   >> ${BUILD_LOGS}/${DEP_NAME}.make.log 2>&1
 
     touch made.sts
 else
